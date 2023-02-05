32a4f0f07a56e9eaa7d8b86ef437ce122b94bef3
Chris Taylor <shredder.ct@gmail.com>
2023-02-04 23:28:28 -0600

    threading lock

--- GameJam.Engine/World.cs
+++ GameJam.Engine/World.cs
@@ -18,18 +18,27 @@ internal class World : IWorld
 
     public void SetComponent<T>(Entity entity, T value) where T : unmanaged
     {
-        _actionQueue.Enqueue(new SetComponentAction<T>(entity, value, _entityBuckets, _entityBucketMap), 1);
+        lock (_actionQueue)
+        {
+            _actionQueue.Enqueue(new SetComponentAction<T>(entity, value, _entityBuckets, _entityBucketMap), 1);
+        }
     }
 
     public void RemoveComponent<T>(Entity entity) where T : unmanaged
     {
-        _actionQueue.Enqueue(new RemoveComponentAction<T>(entity, _entityBuckets, _entityBucketMap), 2);
+        lock (_actionQueue)
+        {
+            _actionQueue.Enqueue(new RemoveComponentAction<T>(entity, _entityBuckets, _entityBucketMap), 2);
+        }
     }
 
     public void DestroyEntity(Entity entity)
     {
-        var record = _entityBucketMap[entity];
-        _actionQueue.Enqueue(new DestroyEntityAction(record, _entityBucketMap), 3);
+        lock (_actionQueue)
+        {
+            var record = _entityBucketMap[entity];
+            _actionQueue.Enqueue(new DestroyEntityAction(record, _entityBucketMap), 3);
+        }
     }
 
     public T? GetComponent<T>(Entity entity) where T : unmanaged
